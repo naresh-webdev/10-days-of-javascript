8b6aa979070a264e2b4613ba444cb7ce9a1dd9d8
naresh-webdev <gkmnaresh01@gmail.com>
2022-08-25 19:36:44 +0530

    all arithemetic functions implemented

--- day-9-binary-calc/js/binaryCalculator.js
+++ day-9-binary-calc/js/binaryCalculator.js
@@ -32,52 +32,88 @@ btns.forEach((btn) => {
       const resValue = res.textContent;
 
       if (resValue.indexOf("+") !== -1) {
-        // Addition operation
+        //* Addition operation
         const iAdd = resValue.indexOf("+");
-        const operand1 = resValue.slice(0, iAdd);
-        const operand2 = resValue.slice(iAdd + 1);
+        const bOperand1 = resValue.slice(0, iAdd);
+        const bOperand2 = resValue.slice(iAdd + 1);
 
         // binary to decimal conversion
+        const dOperand1 = binaryToDecimal(bOperand1);
+        const dOperand2 = binaryToDecimal(bOperand2);
+
+        console.log(dOperand1, dOperand2);
 
         // decimal operation
+        const dSum = dOperand1 + dOperand2;
+        console.log(dSum);
 
         // decimal to binary convertion
+        const bSum = decimalToBinary(dSum);
+
+        // Result
+        res.textContent = bSum;
 
         //
       } else if (resValue.indexOf("-") !== -1) {
+        //* Subtraction
+
         const iSub = resValue.indexOf("-");
-        const operand1 = resValue.slice(0, iSub);
-        const operand2 = resValue.slice(iSub + 1);
+        const bOperand1 = resValue.slice(0, iSub);
+        const bOperand2 = resValue.slice(iSub + 1);
 
         // binary to decimal conversion
+        const dOperand1 = binaryToDecimal(bOperand1);
+        const dOperand2 = binaryToDecimal(bOperand2);
 
         // decimal operation
+        const dSub = dOperand1 - dOperand2;
 
         // decimal to binary convertion
+        const bSub = decimalToBinary(dSub);
+
+        // display result
+        res.textContent = bSub;
 
         //
       } else if (resValue.indexOf("*") !== -1) {
+        //* Multiplication
+
         const iMul = resValue.indexOf("*");
-        const operand1 = resValue.slice(0, iMul);
-        const operand2 = resValue.slice(iMul + 1);
+        const bOperand1 = resValue.slice(0, iMul);
+        const bOperand2 = resValue.slice(iMul + 1);
 
         // binary to decimal conversion
+        const dOperand1 = binaryToDecimal(bOperand1);
+        const dOperand2 = binaryToDecimal(bOperand2);
 
         // decimal operation
+        const dMul = dOperand1 * dOperand2;
 
         // decimal to binary convertion
+        const bMul = decimalToBinary(dMul);
+
+        // Result
+        res.textContent = bMul;
 
         //
       } else if (resValue.indexOf("/") !== -1) {
+        //* Division
         const iDiv = resValue.indexOf("/");
-        const operand1 = resValue.slice(0, iDiv);
-        const operand2 = resValue.slice(iDiv + 1);
+        const bOperand1 = resValue.slice(0, iDiv);
+        const bOperand2 = resValue.slice(iDiv + 1);
 
         // binary to decimal conversion
+        const dOperand1 = binaryToDecimal(bOperand1);
+        const dOperand2 = binaryToDecimal(bOperand2);
 
         // decimal operation
+        const dDiv = dOperand1 / dOperand2;
 
         // decimal to binary convertion
+        const bDiv = decimalToBinary(dMul);
+
+        // Result
+        res.textContent = bDiv;
 
         //
       } else {
@@ -86,3 +122,13 @@ btns.forEach((btn) => {
     }
   });
 });
+
+const binaryToDecimal = function (binary) {
+  const decimal = parseInt(binary, 2);
+  return decimal;
+};
+
+const decimalToBinary = function (decimal) {
+  const binary = Number(decimal).toString(2);
+  return binary;
+};
